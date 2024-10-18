4b2a442403e8dc8c773c8be9e06987cf043acc54
Taras Hoshko <124066115+tarashoshko@users.noreply.github.com>
2024-10-18 14:14:10 +0300

    Update variables.tf

--- terraform-project/variables.tf
+++ terraform-project/variables.tf
@@ -11,51 +11,6 @@ variable "aws_secret_key" {
   description = "AWS Secret Key"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-}
-
-variable "public_subnet_cidr_1" {
-  description = "CIDR block for the first public subnet"
-  type        = string
-}
-
-variable "public_subnet_cidr_2" {
-  description = "CIDR block for the second public subnet"
-  type        = string
-}
-
-variable "private_subnet_cidr_1" {
-  description = "CIDR block for the first private subnet"
-  type        = string
-}
-
-variable "private_subnet_cidr_2" {
-  description = "CIDR block for the second private subnet"
-  type        = string
-}
-
-variable "private_availability_zone_1" {
-  description = "Availability zone for the first private subnet"
-  type        = string
-}
-
-variable "private_availability_zone_2" {
-  description = "Availability zone for the second private subnet"
-  type        = string
-}
-
-variable "public_availability_zone_1" {
-  description = "Availability zone for the first public subnet"
-  type        = string
-}
-
-variable "public_availability_zone_2" {
-  description = "Availability zone for the second public subnet"
-  type        = string
-}
-
 variable "db_username" {
   description = "Database master username"
   type        = string
@@ -66,25 +21,10 @@ variable "db_password" {
   type        = string
 }
 
-variable "db_name" {
-  description = "Name of the database"
-  type        = string
-}
-
-variable "db_subnet_group_name" {
-  description = "Name of the DocumentDB subnet group"
-  type        = string
-}
-
 variable "docker_username" {
   type = string
 }
 
 variable "docker_password" {
   type = string
-}
-
-variable "app_version" {
-  type        = string
-  description = "The app version"
 }
